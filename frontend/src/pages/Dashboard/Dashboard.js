aaa391d4de31d1a78a1cd33f2e81fdb94062c2ad
huuthai1998 <tnguy422@ucsc.edu>
2021-04-19 16:03:50 +0700

    added pages and component

---
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className="bg-blue-600 h-screen text-white flex items-center">
+      <h1 className="text-center font-bold text-5xl w-full">Dashboard Page</h1>
+    </div>
+  )
+}
+
+export default Dashboard
