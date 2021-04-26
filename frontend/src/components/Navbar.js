379240ea19571662437a591a25104b8861243e20
mlee140 <mlee140@users.noreply.github.com>
2021-04-25 23:55:27 -0700

    installed react-router-dom

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,11 +11,11 @@ const Navbar = () => {
             >
                 <Nav 
                     style={{display: 'flex', justifyContent: 'flex-end'}} 
-                    className="px-6 pt-6 pb-2"
+                    className="px-6 pt-6 pb-2 font-sans"
                     activeKey="/home"
                 >
                     <Nav.Item className="px-4">
-                        <Nav.Link href="/home">Dashboard</Nav.Link>
+                        <Nav.Link href="/pages/Dashboard">Dashboard</Nav.Link>
                     </Nav.Item>
                     <Nav.Item className="px-4">
                         <Nav.Link eventKey="link-1">New Drink</Nav.Link>
