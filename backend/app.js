106aa14ea2e1b280a9aa126dd76baec1469b98bc
Jarod Alvarez <xjrdalvarezx@gmail.com>
2021-04-26 19:28:37 -0700

    Updated backend and API

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const yaml = require('js-yaml');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+
+
+const apiSpec = path.join(__dirname, './api/openapi.yaml');
+
+const apidoc = yaml.load(fs.readFileSync(apiSpec, 'utf8'));
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,12 +26,17 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/v0/api-docs', swaggerUi.serve, swaggerUi.setup(apidoc));
+app.listen(3010, () => {
+  console.log("server is listening on 3010");
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +52,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+*/
+//get all bobas
+app.get('/v0/boba', indexRouter.getAll);
+//get a boba
+
+//create a boba
+
+app.post("/v0/boba", indexRouter.post);
+
+//update a boba
+
+//delete a boba
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,77 @@
 var express = require('express');
+const { restart } = require('nodemon');
+const app = require('../app');
 var router = express.Router();
+const pool = require("../db");
+
+//app.use(express.json()) // => req.body
+
+//ROUTES//
+
+//get all bobas
+router.getAll = async (req, res) => {
+  const bobas = await selectBoba();
+  res.status(200).json(bobas);
+}
+//get a boba
+router.getOne = async (req, res) => {
+  const boba = await selectID(req.params.boba_id);
+  if (boba == undefined) {
+    res.status(404).send();
+  } else {
+    res.status(200).json(boba);
+  }
+}
+//create a boba
+router.post = async (req, res) => {
+  try {
+    console.log(req.body);
+    const outBoba = {
+      'boba_id': req.body.bobaid,
+      'price': req.body.price,
+      'drinkname': req.body.drinkname,
+      'description': req.body.description,
+    };
+    await insertBoba(outBoba);
+    res.status(201).send(outBoba);
+  } catch (err) {
+    console.error(err.message);
+    res.status(404).send();
+  }
+}
+
+//update a boba
+
+//delete a boba
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//-- ROUTE HANDLERS---------------------------
+selectBoba = async() => {
+  const select = 'SELECT * FROM boba_entries'
+  const {rows} = await pool.query(select);
+  return rows;
+};
+
+selectID = async(boba_id) => {
+  const select = 'SELECT * FROM boba_entries WHERE boba_id = $1';
+  const query = {
+    text: select,
+    values: [boba_id],
+  };
+
+};
+
+insertBoba = async(boba) => {
+  const insert = 'INSERT INTO boba_entries(boba_id, price, drinkname, description) VALUES ($1, $2, $3, $4)';
+  const query = {
+    text: insert,
+    values: [boba.boba_id, boba.price, boba.drinkname, boba.description]
+  };
+  await pool.query(query);  
+};
 
 module.exports = router;
