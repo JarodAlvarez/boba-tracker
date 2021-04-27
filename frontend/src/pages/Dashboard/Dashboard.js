59c30ba54c9aad281c42dcaca24a77331eedb284
mlee140 <mlee140@users.noreply.github.com>
2021-04-27 16:05:18 -0700

    no changes made

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const Dashboard = () => {
-  return (
-    <div className="bg-blue-600 h-screen text-white flex items-center">
-      <h1 className="text-center font-bold text-5xl w-full">Dashboard Page</h1>
-    </div>
-  )
+    return (
+        <div class="flex justify-evenly m-10 text-center ">
+            <div class = "border-2 border-indigo-600 bg-blue-200 rounded-lg p-6 m-2 w-1/6">
+                <div>Drinks Bought</div>
+                <div># A lot</div>
+                
+            </div>
+            <div class = "border-2 border-yellow-600 bg-yellow-200 rounded-lg p-6 m-2 w-1/6">
+                <div>Money Spent</div>
+                <div>$too much$</div>
+            </div>
+            <div class = "border-2 border-red-600 bg-red-200 rounded-lg p-6 m-2 w-1/6">
+                <div>Drink Sugar Level</div>
+                <div>diabetic levels</div>
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard
