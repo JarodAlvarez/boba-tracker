3082c7324c3bd48427b33c812ae217f5a0dd9a0b
mlee140 <mlee140@users.noreply.github.com>
2021-05-05 02:10:37 -0700

    dashboard done with dummy data, good to show to TA

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -26,13 +26,15 @@ const Dashboard = () => {
             )
     }, []) // empty array so this will only run once
 
-    // calc total of $$ spent
+    // calc total of $$ spent and suagr consumed
     let i;
     var price = 0.0;
+    var sugar = 0.0
     for (i in bobas) {
         price = Number(price) + Number(bobas[i].price);
+        sugar = Number(sugar) + Number(bobas[i].sweetness)
         price = price.toFixed(2);
-        console.log(price);
+        sugar = sugar.toFixed(2);
     }
 
 
@@ -47,24 +49,21 @@ const Dashboard = () => {
                 <div class="flex justify-evenly m-10 text-center ">
                     <div
                         class="border-2 border-indigo-600 bg-blue-200 rounded-lg p-6 m-2 w-1/6 cursor-pointer"
-                        onClick={e => console.log("Clicked")} // change later
                     >
                         <div>Drinks Bought</div>
                         <div>{drinksBought}</div>
-                        {console.log(price)}
+                        {/* {console.log(price)} */}
 
                     </div>
                     <div class="border-2 border-yellow-600 bg-yellow-200 rounded-lg p-6 m-2 w-1/6 cursor-pointer"
-                        onClick={e => console.log("Clicked")} // change later
                     >
                         <div>Money Spent</div>
                         <div>{price}</div>
                     </div>
                     <div class="border-2 border-red-600 bg-red-200 rounded-lg p-6 m-2 w-1/6 cursor-pointer"
-                        onClick={e => console.log("Clicked")} // change later
                     >
-                        <div>Drink Sugar Level</div>
-                        <div>diabetic levels</div>
+                        <div>Sugar consumed</div>
+                        <div>{sugar}</div>
                     </div>
 
                 </div>
