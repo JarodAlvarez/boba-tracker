6be9a81a7347826bca0010a819b7bc300de57ece
mlee140 <mlee140@users.noreply.github.com>
2021-05-10 16:27:57 -0700

    working on spending chart

--- frontend/src/components/spendingChart.js
+++ frontend/src/components/spendingChart.js
@@ -3,25 +3,64 @@ import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
 export default class SpendingChart extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            bobas: []
+        };
+    }
+
     chartRef = React.createRef();
 
     componentDidMount() {
-        const myChartRef = this.chartRef.current.getContext("2d");
+        var date_spendings = [];
 
+        fetch("http://localhost:3010/v0/boba")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    for(var i in result) {
+                        var date = new Date(result[i].purchase_date);
+                        var index = Number(date.getDay());
+                        date_spendings[index] = result[i].price;
+                    }
+                    this.setState({
+                        isLoaded: true,
+                        bobas: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+
+
+        const myChartRef = this.chartRef.current.getContext("2d");
+        console.log(date_spendings);
         new Chart(myChartRef, {
             type: 'line',
             data: {
-                labels: ['5 weeks ago', '4 weeks ago', '3 weeks ago', '2 weeks ago', 'Last week', 'This week'],
+                labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
                 datasets: [{
-                    label: 'spending',
-                    data: [
-                        25,
-                        20,
-                        31.50,
-                        11,
-                        14,
-                        21
-                    ],
+                    label: ' $ Spending',
+                    // data: [
+                    //     25,
+                    //     20,
+                    //     31.50,
+                    //     11,
+                    //     14,
+                    //     21
+                    // ],
+                    data: date_spendings,
                     backgroundColor: [
                         'rgba(236, 220, 194, 0.7)'
                     ],
