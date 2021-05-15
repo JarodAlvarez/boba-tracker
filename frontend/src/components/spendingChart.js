e9ce03da7807c47ee796b42e8bdbfff001283e1b
mlee140 <mlee140@users.noreply.github.com>
2021-05-14 20:33:50 -0700

    changed to bar graph and got data on there

--- frontend/src/components/spendingChart.js
+++ frontend/src/components/spendingChart.js
@@ -1,6 +1,7 @@
-// import React, { Component } from 'react'
-// import { Chart, registerables } from "chart.js";
-// Chart.register(...registerables);
+import React, { useEffect, useRef, useState } from "react";
+import { Chart, registerables } from "chart.js";
+import { useAuth } from 'contexts/authContext'
+Chart.register(...registerables);
 
 // const SpendingChart = () => {
 
@@ -111,62 +112,133 @@
 
 // export default SpendingChart
 
-import React, { useEffect, useRef, useState } from "react";
-import { Chart, registerables } from "chart.js";
-Chart.register(...registerables);
 
 
-const chartConfig = {
-    type: 'line',
-    data: {
-        labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-        datasets: [{
-            label: ' $ Spending',
-            data: [
-                2,
-                20,
-                31.50,
-                11,
-                14,
-                21
-            ],
-            // data: date_spendings,
-            backgroundColor: [
-                'rgba(236, 220, 194, 0.7)'
-            ],
-            borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)"
-            ],
-            borderWidth: 1
-        }
-        ]
-    },
-    options: {
-        scales: {
-            yAxes: [
-                {
-                    ticks: {
-                        beginAtZero: false
-                    }
-                }
-            ]
-        }
-    }
-};
+
+// const chartConfig = {
+//     type: 'line',
+//     data: {
+//         labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+//         datasets: [{
+//             label: ' $ Spending',
+//             data: [
+//                 2,
+//                 20,
+//                 31.50,
+//                 11,
+//                 14,
+//                 21
+//             ],
+//             // data: date_spendings,
+//             backgroundColor: [
+//                 'rgba(236, 220, 194, 0.7)'
+//             ],
+//             borderColor: [
+//                 "rgba(255, 99, 132, 1)",
+//                 "rgba(54, 162, 235, 1)",
+//                 "rgba(255, 206, 86, 1)",
+//                 "rgba(75, 192, 192, 1)",
+//                 "rgba(153, 102, 255, 1)",
+//                 "rgba(255, 159, 64, 1)"
+//             ],
+//             borderWidth: 1
+//         }
+//         ]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [
+//                 {
+//                     ticks: {
+//                         beginAtZero: false
+//                     }
+//                 }
+//             ]
+//         }
+//     }
+// };
 
 const SpendingChart = () => {
     const chartContainer = useRef(null);
     const [chartInstance, setChartInstance] = useState(null);
+    const [error, setError] = React.useState(null);
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    const [bobas, setBobas] = React.useState([]);
+    const authContext = useAuth();
+    const call = "http://localhost:3010/v0/boba/" + authContext.authContext.user.email;
+    var date_spendings = [0,0,0,0,0,0,0];
 
     useEffect(() => {
+        fetch(call)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    for (var i in result) {
+                        var date = new Date(result[i].purchase_date);
+                        var index = Number(date.getDay());
+                        date_spendings[index] = date_spendings[index] + result[i].price;
+                    }
+                    setIsLoaded(true);
+                    setBobas(result);
+                    console.log(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+
+        const chartConfig = {
+            type: 'bar',
+            data: {
+                labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+                datasets: [{
+                    label: ' $ Spending',
+                    // data: [
+                    //     2,
+                    //     20,
+                    //     31.50,
+                    //     11,
+                    //     14,
+                    //     21
+                    // ],
+                    data: date_spendings,
+                    backgroundColor: [
+                        'rgba(236, 220, 194, 0.7)'
+                    ],
+                    borderColor: [
+                        "rgba(255, 99, 132, 1)",
+                        "rgba(54, 162, 235, 1)",
+                        "rgba(255, 206, 86, 1)",
+                        "rgba(75, 192, 192, 1)",
+                        "rgba(153, 102, 255, 1)",
+                        "rgba(255, 159, 64, 1)"
+                    ],
+                    borderWidth: 1
+                }
+                ]
+            },
+            options: {
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
+                                beginAtZero: false
+                            }
+                        }
+                    ]
+                }
+            }
+        };
+
+
         if (chartContainer && chartContainer.current) {
             const newChartInstance = new Chart(chartContainer.current, chartConfig);
             setChartInstance(newChartInstance);
+            newChartInstance.update();
         }
     }, [chartContainer]);
 
@@ -174,7 +246,6 @@ const SpendingChart = () => {
         chartInstance.data.datasets[datasetIndex].data = newData;
         chartInstance.update();
     };
-
 
     return (
         <div>
